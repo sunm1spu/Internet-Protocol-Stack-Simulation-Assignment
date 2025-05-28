b0f251791ca3600665971a132cda62f766f230a1
Usagi Mero <usagimero@gmail.com>
2025-05-27 22:17:28 -0700

    Link and Client update

--- client/client.h
+++ client/client.h
@@ -3,18 +3,22 @@
 #define CLIENT_H
 
 #include <string>
+#include <stack>
+
 using std::string;
+using std::stack;
 
 class Client {
     public:
         Client(string dwIP); // default constructor
         void SendRequest(string dwHost);
-        void ProcessRequests();
+        void ProcessIncomingRequests();
         void RecieveRequest();
 
     private:
         string m_dwIP;
         string m_dwMACAddress;
+        stack<
 };
 
 #endif
--- layers/link.h
+++ layers/link.h
@@ -1,1 +1,9 @@
+class Link {
+    public:
+        Link();
+        string Encapsulate(string message);
+        string Decapsulate(string message);
 
+    private:
+
+};
