bce1cd63ee74c6d96b19bc9cc8fd36770fe04db0
Usagi Mero <usagimero@gmail.com>
2025-05-27 21:31:36 -0700

    client updated

--- client/client.h
+++ client/client.h
@@ -1,7 +1,20 @@
 
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#include <string>
+using std::string;
+
 class Client {
     public:
+        Client(); // default constructor
+        void SendRequest(string host, string method, string body);
+        void ProcessRequests();
+        void RecieveRequest();
 
     private:
-    
-}
+        string m_dwIP;
+        string m_dwMACAddress;
+};
+
+#endif
