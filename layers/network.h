75afdfdc183ece9ac48451d82bf3e877be0efdec
Usagi Mero <usagimero@gmail.com>
2025-05-27 22:46:21 -0700

    Edit network.h

--- layers/network.h
+++ layers/network.h
@@ -1,1 +1,19 @@
 
+#ifndef NETWORK_H
+#define NETWORK_H
+
+#include <string>
+
+using std::string;
+
+class NetworkLayer {
+    public:
+        NetworkLayer();
+        string Encapsulate(string message);
+        string Decapsulate(string message);
+
+    private:
+
+};
+
+#endif
