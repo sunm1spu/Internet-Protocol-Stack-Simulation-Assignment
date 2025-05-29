974bf3d12d5f4453355833575185cf87786df7b1
Usagi Mero <usagimero@gmail.com>
2025-05-29 03:54:52 -0700

    Fix tasks.json finally

--- main.cpp
+++ main.cpp
@@ -9,6 +9,8 @@ using std::endl;
 int main(int argc, char const *argv[])
 {
 
+    cout << "Test" << endl;
+    
     Client* c1 = new Client("192.168.056.001"); 
     Client* c2 = new Client("172.031.000.001"); 
 
